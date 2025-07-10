402cea9f53bae46abf27d1f9ffcee4a91b8fc31c
Vaiperdix <116468676+Vaiperdix@users.noreply.github.com>
2025-07-10 08:18:54 -0400

    app edited

--- app.py
+++ app.py
@@ -1,18 +1,58 @@
 from flask import Flask, request, jsonify
-import prediction  # Tu script de predicción
+from ultralytics import YOLO
+import json
+import base64
+from io import BytesIO
+from PIL import Image
 
 app = Flask(__name__)
 
-@app.route('/predict', methods=['POST'])
-def predict_endpoint():
-    # Recibir JSON del frontend
-    data = request.json
-    
-    # Llamar a tu función de predicción
-    results = prediction.Predict(data)
-    
-    # Retornar array de floats
-    return jsonify({"predictions": results.tolist()})
+@app.route('/', methods=['POST'])
+def predict():
+    try:
+        print("Starting prediction...")
+        # Recibir JSON del frontend
+        data = request.json
+        
+        # get payload
+        body = json.loads(data['body'])
+
+        # get params
+        img_b64 = body['image']
+        version = body['version']
+        print("version: ", version)
+
+        # open image
+        img = Image.open(BytesIO(base64.b64decode(img_b64.encode('ascii'))))
+
+        model_map = {
+            "n": "./models/yolo11n-pose.pt",
+            "s": "./models/yolo11s-pose.pt",
+            "m": "./models/yolo11m-pose.pt",
+            "l": "./models/yolo11l-pose.pt",
+            "x": "./models/yolo11x-pose.pt"
+        }
+
+        model = YOLO(model_map.get(version, "./models/yolo11n-pose.pt"))
+        
+        results = model(img)
+        
+        for result in results:
+            print("\nInformación de la detección:")
+            print(f"Número de personas detectadas: {len(result.boxes)}")
+            if len(result.boxes) > 0:
+                print("\nDetalles de la primera persona:")
+                print(f"Coordenadas del bounding box: {result.boxes[0].xyxy}")
+                print(f"Puntos clave detectados: {result.keypoints[0].xy}")
+                print(f"Confianza de la detección: {result.boxes[0].conf}")
+        
+        annotated_frame = results[0].plot()
+        print("Results: ", annotated_frame)
+        print("Ending prediction...")
+        return annotated_frame
+    except Exception as e:
+        print("Error:", e)
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
